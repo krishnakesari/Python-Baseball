cf1014a174f87ad9cfc4cf67e12f30eb55cc31b9
Krishnakesari <bkesari@worldbank.org>
2021-12-30 17:25:15 -0500

    updated game data frame for testing

--- stats/data.py
+++ stats/data.py
@@ -2,7 +2,7 @@ import os
 import glob
 import pandas as pd
 
-game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
+game_files = glob.glob(os.path.join(os.getcwd(), '..', 'games', '*.EVE'))
 game_files.sort()
 
 game_frames = []
@@ -12,7 +12,7 @@ for game_file in game_files:
     game_frames.append(game_frame)
     games = pd.concat(game_frames)
 
-games.loc[games['multi5'] == '??', games['multi5']] = ''
+games.loc[games['multi5'] == '??'] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
@@ -22,3 +22,5 @@ games = pd.concat([games, identifiers], axis=1, sort=False)
 games = games.fillna(' ')
 
 games.loc[:, 'type'] = pd.Categorical(games['type'])
+
+games.head()
